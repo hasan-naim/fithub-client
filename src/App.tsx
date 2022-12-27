30bf8a3c68e2f42899cb19e76a4275cde5429fca
hasan-naim <hasannaim6246@gmail.com>
2022-12-27 09:54:21 +0600

    router setup is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/Routes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
